326c808bb293d0963f159729df98e6be485b3c96
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-20 23:40:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,7 +115,7 @@ async def start(client, message):
             f_caption=msg.get("caption", "")
             if BATCH_FILE_CAPTION:
                 try:
-                    f_caption=BATCH_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                    f_caption=BATCH_FILE_CAPTION.format(file_name= "" if title is None else title, file_size= "" if size is None else size, file_caption= "" if f_caption is None else f_caption)
                 except Exception as e:
                     logger.exception(e)
                     f_caption=f_caption
